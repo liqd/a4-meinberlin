9d49c2963f15e2c39d7232699404227f7b56a4b3
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-09-21 17:02:44 +0200

    Filter containered projects from the project list

--- meinberlin/apps/projects/views.py
+++ meinberlin/apps/projects/views.py
@@ -119,9 +119,9 @@ class ProjectListView(filter_views.FilteredListView):
 
     def get_queryset(self):
         return super().get_queryset().filter(
-            Q(is_draft=False, is_public=False,
-              participants__pk=self.request.user.pk) |
-            Q(is_draft=False, is_public=False,
-              organisation__initiators__pk=self.request.user.pk) |
-            Q(is_draft=False, is_public=True)
+            Q(is_draft=False) & (
+                Q(is_public=True) |
+                Q(participants__pk=self.request.user.pk) |
+                Q(organisation__initiators__pk=self.request.user.pk)
+            ) & Q(containers=None)
         ).distinct()

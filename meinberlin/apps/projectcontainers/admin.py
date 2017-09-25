3cf3215fa2e4576570acecfd259192a9cfab9cd0
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-09-25 09:36:35 +0200

    Add basic container project component

--- meinberlin/apps/projectcontainers/admin.py
+++ meinberlin/apps/projectcontainers/admin.py
@@ -5,7 +5,7 @@ from meinberlin.apps.projectcontainers import models
 
 @admin.register(models.ProjectContainer)
 class ProjectContainerAdmin(admin.ModelAdmin):
-    fields = ('name', 'organisation',
+    fields = ('name', 'organisation', 'description',
               'tile_image', 'tile_image_copyright',
               'is_draft', 'is_archived',
               'projects',

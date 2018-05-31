0aec44d893793f8a3e6be743b742661efe69498a
Irae Hueck Costa <mail@irae.me>
2018-05-31 17:59:01 +0200

    Admin stuff

--- meinberlin/apps/newsletters/admin.py
+++ meinberlin/apps/newsletters/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 from adhocracy4.projects.admin import ProjectAdminFilter
+from background_task.models_completed import CompletedTask
+from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
 
 from . import models
 
@@ -20,3 +23,35 @@ class NewsletterAdmin(admin.ModelAdmin):
                 newsletter.tasks.count())
     errors.allow_tags = True
 
+class NewsletterTask(CompletedTask):
+    class Meta:
+        proxy = True
+
+@admin.register(NewsletterTask)
+class NewsletterTaskAdmin(admin.ModelAdmin):
+    
+    list_display = ['verbose_name', 'newsletter_id', 'newsletter_organisation', 'last_error_line']
+
+    def get_queryset(self, request):
+        #assert 0, request
+        newsletter_content_type = ContentType.objects.get(app_label='meinberlin_newsletters', model='newsletter')
+        qs = super().get_queryset(request).filter(creator_content_type=newsletter_content_type.pk)
+        newsletter_id = request.GET.get('filter_by_creator')
+        if newsletter_id:
+            return qs.filter(creator_object_id=newsletter_id)
+        return qs
+
+    def newsletter_id(self, task):
+        if task.creator:
+            url = reverse('admin:meinberlin_newsletters_newsletter_change', args=[task.creator.id])
+            return '<a href="{}">{}</a>'.format(url, task.creator.id)  # no escaping
+    newsletter_id.allow_tags = True
+
+    def newsletter_organisation(self, task):
+        if task.creator:
+            return task.creator.organisation
+
+    def last_error_line(self, task):
+        lines = task.last_error.splitlines()
+        if lines:
+            return lines[-1]

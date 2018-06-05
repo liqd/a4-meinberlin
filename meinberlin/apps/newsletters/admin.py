fa108f224f09288b14fa88dbeebf9fe4b729939f
Irae Hueck Costa <mail@irae.me>
2018-06-05 12:10:32 +0200

    Some style fixes

--- meinberlin/apps/newsletters/admin.py
+++ meinberlin/apps/newsletters/admin.py
@@ -21,7 +21,7 @@ class NewsletterAdmin(admin.ModelAdmin):
     )
     date_hierarchy = 'sent'
     readonly_fields = ['email_status']
-    
+
     def email_status(self, newsletter):
         queue = newsletter.tasks.count()
         success = newsletter.completed_tasks.filter(last_error='').count()
@@ -30,9 +30,12 @@ class NewsletterAdmin(admin.ModelAdmin):
             queue=queue,
             success=success,
             errors=errors,
-            url=reverse('admin:meinberlin_newsletters_newslettertask_changelist'),
+            url=reverse(
+                'admin:meinberlin_newsletters_newslettertask_changelist'),
             newsletter_id=newsletter.id)
+
     email_status.allow_tags = True
+
 
 # Kind of a hack so we can have tasks two times in the admin interface
 class NewsletterTask(CompletedTask):
@@ -40,21 +43,28 @@ class NewsletterTask(CompletedTask):
         proxy = True
         verbose_name = 'Newsletter send Email'
 
+
 @admin.register(NewsletterTask)
 class NewsletterTaskAdmin(admin.ModelAdmin):
-    
+
     list_display = ['verbose_name', 'newsletter_id', 'last_error_line']
     list_filter = ['creator_object_id']
     readonly_fields = ['raw_email']
 
     def get_queryset(self, request):
-        newsletter_content_type = ContentType.objects.get(app_label='meinberlin_newsletters', model='newsletter')
-        qs = super().get_queryset(request).filter(creator_content_type=newsletter_content_type.pk)
+        newsletter_content_type = ContentType.objects.get(
+            app_label='meinberlin_newsletters', model='newsletter')
+        qs = super().get_queryset(request).filter(
+            creator_content_type=newsletter_content_type.pk)
         return qs
 
     def newsletter_id(self, task):
-        url = reverse('admin:meinberlin_newsletters_newsletter_change', args=[task.creator.id])
-        return '<a href="{}">{}</a>'.format(url, task.creator.id)  # no escaping
+        url = reverse(
+            'admin:meinberlin_newsletters_newsletter_change',
+            args=[task.creator.id])
+        return '<a href="{}">{}</a>'.format(url,
+                                            task.creator.id)  # no escaping
+
     newsletter_id.allow_tags = True
     newsletter_id.admin_order_field = 'creator_object_id'
 
@@ -62,9 +72,10 @@ class NewsletterTaskAdmin(admin.ModelAdmin):
         lines = task.last_error.splitlines()
         if lines:
             return lines[-1]
+
     last_error_line.admin_order_field = 'last_error'
 
     def raw_email(self, task):
         task_args, task_kwargs = json.loads(task.task_params)
         email = deserialize_email(task_args[0])
-        return  email.message()
+        return email.message()

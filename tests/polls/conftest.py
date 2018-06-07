e029c941fa2b483550f05c7edef2f6686c739ac4
Irae Hueck Costa <mail@irae.me>
2018-06-07 10:57:30 +0200

    Moved mb helpers to a4

--- tests/polls/conftest.py
+++
@@ -1,8 +0,0 @@
-from pytest_factoryboy import register
-
-from meinberlin.test.factories import polls
-
-register(polls.PollFactory)
-register(polls.QuestionFactory)
-register(polls.ChoiceFactory)
-register(polls.VoteFactory)

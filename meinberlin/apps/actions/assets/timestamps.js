liqd/adhocracy4

commit 6d9a62232895d2cba4a6a842c857384cf6889f6d

assets/timestamps.js: Use minified version of timeago

meinberlin/apps/actions/assets/timestamps.js
@@ -1,5 +1,4 @@
-const timeago = require('timeago.js')
-require('timeago.js/src/timeago.locales.js')
+const timeago = require('timeago.js/dist/timeago.min.js')
 const $ = require('jquery')
 
 $(function () {

liqd/a4-meinberlin

commit 97f485ad9c9a1ab3b9eeaa0417209805d4df6a79

apps/actions: Fix timeago call
This broke in 6d9a62232895d2cba or the commit before.

meinberlin/apps/actions/assets/timestamps.js
@@ -7,10 +7,9 @@ $(function () {
   $times.each((i, e) => {
     const sevenDays = 60 * 60 * 24 * 7 * 1000
     const datetime = new Date($(e).attr('datetime'))
-    const timeagoInstance = timeago()
 
     if ((new Date() - datetime) < sevenDays) {
-      e.textContent = timeagoInstance.format(datetime, 'de')
+      e.textContent = timeago.format(datetime, 'de')
     }
   })
 })
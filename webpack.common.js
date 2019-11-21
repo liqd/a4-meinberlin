adebc183fa09886dfb0b7035c86220cbb7749685
Robert Mader <r.mader@liqd.net>
2019-11-21 17:38:16 +0100

    WIP use vendor chunk

--- webpack.common.js
+++ webpack.common.js
@@ -11,48 +11,19 @@ module.exports = {
       'shariff/dist/shariff.min.css',
       './meinberlin/assets/js/app.js'
     ],
-    vendor: [
-      'classnames',
-      '@fortawesome/fontawesome-free/scss/fontawesome.scss',
-      '@fortawesome/fontawesome-free/scss/brands.scss',
-      '@fortawesome/fontawesome-free/scss/regular.scss',
-      '@fortawesome/fontawesome-free/scss/solid.scss',
-      'js-cookie',
-      'react',
-      'immutability-helper',
-      'react-dom',
-      'react-flip-move',
-      'react-sticky-box',
-      'slick-carousel/slick/slick.min.js',
-      'slick-carousel/slick/slick.css'
-    ],
     mb_plans_map: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
-      'leaflet.markercluster/dist/MarkerCluster.css',
-      'react-bootstrap-typeahead/css/Typeahead.css',
       './meinberlin/apps/plans/assets/plans_map.jsx'
     ],
     a4maps_display_point: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
       'adhocracy4/adhocracy4/maps/static/a4maps/a4maps_display_point.js'
     ],
     a4maps_display_points: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
-      'leaflet.markercluster/dist/MarkerCluster.css',
       'adhocracy4/adhocracy4/maps/static/a4maps/a4maps_display_points.js'
     ],
     a4maps_choose_point: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
       'adhocracy4/adhocracy4/maps/static/a4maps/a4maps_choose_point.js'
     ],
     a4maps_choose_polygon: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
-      'leaflet-draw/dist/leaflet.draw.css',
       './meinberlin/apps/maps/assets/map_choose_polygon_with_preset.js'
     ],
     datepicker: [
@@ -67,6 +38,12 @@ module.exports = {
     ],
     select2: [
       'select2'
+    ],
+    unload_warning: [
+      './meinberlin/apps/contrib/static/js/unload_warning.js'
+    ],
+    imageUploader: [
+      'adhocracy4/adhocracy4/images/static/a4images/imageUploader.js'
     ]
   },
   output: {
@@ -150,10 +127,6 @@ module.exports = {
       jQuery: 'jquery',
       Promise: ['es6-promise', 'Promise']
     }),
-    new webpack.optimize.SplitChunksPlugin({
-      name: 'vendor',
-      filename: 'vendor.js'
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[name].css'
@@ -170,5 +143,24 @@ module.exports = {
         flatten: false
       }
     ])
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        leaflet: {
+          test: /[\\/]node_modules[\\/](leaflet|leaflet-draw|leaflet.markercluster|mapbox-gl|mapbox-gl-leaflet)[\\/]/,
+          name: 'leaflet',
+          chunks: 'all',
+          priority: 0
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          priority: -100,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
 }

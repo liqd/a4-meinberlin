e4cb9be1601b638d6163580b9dbf3bc4d87145b8
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-12-13 17:33:26 +0100

    release commit - apps/moderationtasks: remove moderator tasks from dashboard, detail view and form

--- meinberlin/apps/moderationtasks/dashboard.py
+++ meinberlin/apps/moderationtasks/dashboard.py
@@ -17,7 +17,7 @@ class ModerationTasksComponent(ModuleFormSetComponent):
         'meinberlin_moderationtasks/moderation_tasks_form.html'
 
     def is_effective(self, module):
-        return module.blueprint_type in ['PB', 'PB2', 'PB3']
+        return False
 
 
 components.register_module(ModerationTasksComponent())

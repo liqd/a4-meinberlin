415ffcb24fed9d5aa7b4957f110b830ab9d4e362
philli-m <5871230+philli-m@users.noreply.github.com>
2023-09-19 15:42:31 +0200

    cms/header: add BO header template and logos, add snippet for a11y info, rm old styling

--- meinberlin/apps/cms/models/settings.py
+++ meinberlin/apps/cms/models/settings.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import MultiFieldPanel
+from wagtail.admin.panels import ObjectList
+from wagtail.admin.panels import PageChooserPanel
+from wagtail.admin.panels import TabbedInterface
 from wagtail.contrib.settings.models import BaseSiteSetting
 from wagtail.contrib.settings.models import register_setting
 
@@ -26,3 +31,55 @@ class HeaderPages(BaseSiteSetting):
     )
 
     panels = [FieldPanel("help_page"), FieldPanel("feedback_page")]
+
+
+@register_setting
+class Accessibility(BaseSiteSetting):
+    feedback_name = models.CharField(max_length=255, blank=True)
+    feedback_email = models.EmailField(max_length=255, blank=True)
+    feedback_phone = models.CharField(max_length=255, blank=True)
+
+    additional_external_info = models.URLField(
+        help_text=_("Add link to further information"), blank=True
+    )
+
+    additional_external_info_title = models.CharField(max_length=255, blank=True)
+
+    accessibility_declaration = models.ForeignKey(
+        "wagtailcore.Page",
+        related_name="accessibility_declaration_page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+
+    accessibility_declaration_title = models.CharField(max_length=255, blank=True)
+
+    content_panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("feedback_name"),
+                FieldPanel("feedback_email"),
+                FieldPanel("feedback_phone"),
+            ],
+            heading=_("Accessibility Feedback Contact Information"),
+        ),
+        MultiFieldPanel(
+            [
+                PageChooserPanel("accessibility_declaration", ["apps_home.SimplePage"]),
+                FieldPanel("accessibility_declaration_title"),
+            ],
+            heading="Accessibility Declaration",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("additional_external_info"),
+                FieldPanel("additional_external_info_title"),
+            ],
+            heading=_("Further Information on Accessibility"),
+        ),
+    ]
+
+    edit_handler = TabbedInterface(
+        [ObjectList(content_panels, heading=_("Accessibility Information"))]
+    )

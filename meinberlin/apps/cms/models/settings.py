0e9a3c1d068965c383e6f2377c994841f87e464d
philli-m <5871230+philli-m@users.noreply.github.com>
2023-09-19 15:42:31 +0200

    cms/header: add BO header template and logos, a11y info to header pages setting, rm old styling

--- meinberlin/apps/cms/models/settings.py
+++ meinberlin/apps/cms/models/settings.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import MultiFieldPanel
+from wagtail.admin.panels import ObjectList
+from wagtail.admin.panels import PageChooserPanel
+from wagtail.admin.panels import TabbedInterface
 from wagtail.contrib.settings.models import BaseSiteSetting
 from wagtail.contrib.settings.models import register_setting
 
@@ -13,7 +18,7 @@ class HeaderPages(BaseSiteSetting):
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        help_text="Please add a link to the help page.",
+        help_text=_("Please add a link to the help page."),
     )
     feedback_page = models.ForeignKey(
         "wagtailcore.Page",
@@ -22,7 +27,50 @@ class HeaderPages(BaseSiteSetting):
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        help_text="Please add a link to the feedback form page.",
+        help_text=_("Please add a link to the feedback form page."),
     )
+    # accessibility settings
+    feedback_name = models.CharField(max_length=255, blank=True)
+    feedback_email = models.EmailField(max_length=255, blank=True)
+    feedback_phone = models.CharField(max_length=255, blank=True)
+    additional_external_info = models.URLField(
+        help_text=_("Add link for additional information"), blank=True
+    )
+    additional_external_info_link_text = models.CharField(max_length=255, blank=True)
+    accessibility_declaration = models.ForeignKey(
+        "wagtailcore.Page",
+        related_name="accessibility_declaration_page",
+        verbose_name="Accessibility Decleration Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text=_("Please add a link to your accesibility decleration page."),
+    )
+    accessibility_declaration_link_text = models.CharField(max_length=255, blank=True)
 
-    panels = [FieldPanel("help_page"), FieldPanel("feedback_page")]
+    panels = [
+        FieldPanel("help_page"),
+        FieldPanel("feedback_page"),
+        MultiFieldPanel(
+            [
+                FieldPanel("feedback_name"),
+                FieldPanel("feedback_email"),
+                FieldPanel("feedback_phone"),
+            ],
+            heading=_("Accessibility Feedback Contact Information"),
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("accessibility_declaration"),
+                FieldPanel("accessibility_declaration_link_text"),
+            ],
+            heading="Accessibility Declaration",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("additional_external_info"),
+                FieldPanel("additional_external_info_link_text"),
+            ],
+            heading=_("Additional Information on Accessibility"),
+        ),
+    ]

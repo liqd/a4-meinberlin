e5cc37f410c74b5b0df10add1b975da023355ed1
Mara Karagianni <m.karagianni@liqd.net>
2023-10-02 17:50:40 +0300

    documents/views: add extra context in paragraph detail view

--- tests/documents/test_document_api.py
+++ tests/documents/test_document_api.py
@@ -755,3 +755,16 @@ def test_initiator_cant_create_paragraph_image_has_alt_text(apiclient, module):
     paragraph_pk = chapter["paragraphs"][0]["id"]
     paragraph_text = document_models.Paragraph.objects.get(pk=paragraph_pk).text
     assert paragraph_text == 'A beautiful image <img alt="description">'
+
+
+@pytest.mark.django_db
+def test_initiator_retreive_paragraph(apiclient, module, paragraph):
+    project = module.project
+    paragraph = document_models.Paragraph.objects.first()
+    initiator = project.organisation.initiators.first()
+    apiclient.force_authenticate(user=initiator)
+    url = reverse("meinberlin_documents:paragraph-detail", kwargs={"pk": paragraph.pk})
+    response = apiclient.get(url, format="json")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.context["project"] == project
+    assert response.context["chapter"] == paragraph.chapter

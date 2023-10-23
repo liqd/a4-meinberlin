5f8f4bf6f85e97ee9a68073a547e7abd214215fc
Mara Karagianni <m.karagianni@liqd.net>
2023-10-23 15:19:23 +0300

    contrib: add task, update command, add tests

--- meinberlin/config/settings/base.py
+++ meinberlin/config/settings/base.py
@@ -580,7 +580,13 @@ CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 CELERY_RESULT_EXTENDED = True
 
 # BO external footer
-BERLIN_FOOTER_PATH = "landesfooter.inc"
+BERLIN_FOOTER_FILENAME = "landesfooter.inc"
 BERLIN_FOOTER_URL = (
     "https://www.berlin.de/rbmskzl/aktuelles/__i9/std/landesfooter.inc?js=0?css=0"
 )
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}

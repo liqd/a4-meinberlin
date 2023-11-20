b1d3ae7740382e50f3ed17f74b046b80235d338f
Philipp Veller <mail@vellip.de>
2023-11-20 08:29:37 +0100

    react/controlbar: adapt controlbar to new design and updated functionality
    react/controlbar: PR review fixes
    
    react/controlBarSearch: update to reflect markup of styleguide, add some style fixes

--- meinberlin/react/contrib/ControlBar.jsx
+++ meinberlin/react/contrib/ControlBar.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ControlBarDropdown } from './ControlBarDropdown'
 import { ControlBarListMapSwitch } from './ControlBarListMapSwitch'
 import { ControlBarSearch } from './ControlBarSearch'
-import { ControlBarSearchTerm } from './ControlBarSearchTerm'
-import { FilterToggle } from './FilterToggle'
 import django from 'django'
 import { useSearchParams } from 'react-router-dom'
+import { ControlBarFilterPills } from './ControlBarFilterPills'
 
 const translated = {
-  showFilters: django.gettext('Show filters'),
-  hideFilters: django.gettext('Hide filters'),
+  reset: django.gettext('Reset'),
+  showFilters: django.gettext('Show more'),
+  hideFilters: django.gettext('Show less'),
   filters: django.gettext('Filters'),
+  filter: django.gettext('filter'),
   nav: django.gettext('Search, filter and sort the ideas list')
 }
 
@@ -24,44 +25,65 @@ const getResultCountText = (count) => {
 }
 
 export const ControlBar = (props) => {
-  const [expandFilters, setExpandFilters] = useState()
-  const [resultString, setResultString] = useState(false)
+  const [expandFilters, setExpandFilters] = useState(true)
   const [queryParams, setQueryParams] = useSearchParams()
-  const [term, setTerm] = useState(queryParams.get('search') || '')
+  const [filters, setFilters] = useState([])
+  const term = queryParams.get('search') || ''
+  const appliedFilters = filters
+    .concat([{ type: 'search', label: term, value: term }])
+    .filter((f) =>
+      f.type !== 'ordering' &&
+    queryParams?.has(f.type)
+    )
 
-  // check list is filtered not just ordered
-  // else needed for search term deletion
-  const handleResultString = () => {
-    const entryArray = Array.from(queryParams.keys())
-    if (
-      (entryArray.length === 2 && entryArray[1] !== 'ordering') ||
-      entryArray.length > 2
-    ) {
-      setResultString(true)
-    } else {
-      setResultString(false)
+  useEffect(() => {
+    // initialize filters object
+    if (props.filters) {
+      const updatedFilters = Object.keys(props.filters)
+        .map((type) => ({ ...props.filters[type], type, value: queryParams.get(type) }))
+
+      setFilters(updatedFilters)
     }
-  }
+  }, [props.filters])
 
-  const applyFilter = (filterType, filterChoice) => {
-    queryParams.set(filterType, filterChoice[0])
-
-    // to avoid empty pagination page for given
-    // filter settings, always show first page,
+  const applyQuery = (type, value) => {
+    if (value && value !== '') {
+      queryParams.set(type, value)
+    } else {
+      queryParams.delete(type)
+    }
     queryParams.delete('page')
-    handleResultString()
     setQueryParams(queryParams)
   }
 
-  const applySearch = (value) => {
-    setTerm(value)
-    applyFilter('search', [value])
-    handleResultString()
+  const handleSearch = (value) => {
+    applyQuery('search', value)
   }
 
-  const handleToggleFilters = (e) => {
-    e.preventDefault()
-    setExpandFilters(!expandFilters)
+  const handleFilterChange = (type, choice) => {
+    setFilters(filters.map(f => f.type === type ? { ...f, value: choice } : f))
+
+    if (type === 'ordering' || choice === '') {
+      applyQuery(type, choice)
+    }
+  }
+
+  const applyAllFilters = () => {
+    filters.forEach(f => {
+      if (f.value && f.value !== '') {
+        queryParams.set(f.type, f.value)
+      } else {
+        queryParams.delete(f.type)
+      }
+    })
+
+    queryParams.delete('page')
+    setQueryParams(queryParams)
+  }
+
+  const clearFilters = () => {
+    setQueryParams('')
+    setFilters(filters.map(f => ({ ...f, value: '' })))
   }
 
   return (
@@ -69,65 +91,90 @@ export const ControlBar = (props) => {
       className="container u-spacer-bottom u-spacer-top-double"
       aria-label={translated.nav}
     >
-      <div className="offset-lg-2 col-lg-8">
-        <ControlBarListMapSwitch query={queryParams} />
-        <>
-          <div className="control-bar">
-            <div className="control-bar__item">
-              <ControlBarSearch
-                term={term}
-                onSearch={(value) => applySearch(value)}
-              />
-            </div>
-            {props.filters?.ordering && (
-              <ControlBarDropdown
-                key="ordering_dropdown"
-                filter={props.filters.ordering}
-                current={queryParams.get('ordering')}
-                filterId="id_ordering"
-                onSelectFilter={(choice) => applyFilter('ordering', choice)}
-              />
-            )}
-            <div className="control-bar__item control-bar__right">
-              <FilterToggle
-                showFilters={expandFilters}
-                onClickToggleFilter={handleToggleFilters}
-                btnString={translated.filters}
-                showFiltersString={translated.showFilters}
-                hideFiltersString={translated.hideFilters}
-              />
-            </div>
-          </div>
-          {props.filters && expandFilters && (
-            <>
-              <div className="control-bar">
-                {Object.keys(props.filters).map((type, idx) => {
-                  const filterItem = props.filters[type]
-                  return (
-                    type !== 'ordering' && (
-                      <ControlBarDropdown
-                        key={'filter_' + idx}
-                        filter={filterItem}
-                        current={queryParams.get(type)}
-                        filterId={'id_' + type}
-                        onSelectFilter={(choice) => applyFilter(type, choice)}
-                      />
-                    )
-                  )
-                })}
+      <div className="modul-facetingform js-facetingform">
+        <div className="facetingform__container">
+          <div className="container__body">
+            <div className="facets">
+              <fieldset className="facet">
+                <div className="facet__body">
+                  <div className="flexgrid grid--3">
+                    <div className="span2">
+                      <ControlBarSearch onSearch={(value) => handleSearch(value)} term={term} />
+                    </div>
+                    <div className="span1">
+                      {props.filters?.ordering && (
+                        <ControlBarDropdown
+                          key="ordering_dropdown"
+                          filter={props.filters.ordering}
+                          current={queryParams.get('ordering')}
+                          filterId="id_ordering"
+                          onSelectFilter={(choice) => handleFilterChange('ordering', choice[0])}
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset className="facet">
+                <legend className="facet__head">
+                  <span className="facet-title">{translated.filters}</span>
+                </legend>
+                <div className="facet__body">
+                  <div className="flexgrid grid--3" id="filters">
+                    {filters.length > 0 && (
+                      filters
+                        .filter((f) => f.type !== 'ordering')
+                        .slice(0, expandFilters ? filters.length : 3).map((filter) => (
+                          <ControlBarDropdown
+                            key={filter.type}
+                            filter={filter}
+                            current={filter.value}
+                            filterId={'id_' + filter.type}
+                            onSelectFilter={(choice) => handleFilterChange(filter.type, choice[0])}
+                          />
+                        ))
+                    )}
+                  </div>
+                  {filters.length > 3 && (
+                    <button
+                      onClick={() => setExpandFilters(!expandFilters)}
+                      className="control-bar__filter-btn"
+                      aria-controls="filters"
+                      aria-expanded={expandFilters}
+                    >
+                      <span className={'fa fa-chevron-' + (expandFilters ? 'up' : 'down')} aria-hidden="true" />&nbsp;
+                      {expandFilters ? translated.hideFilters : translated.showFilters}
+                    </button>
+                  )}
+                </div>
+              </fieldset>
+              <div className="form-actions">
+                <button className="link form-actions__left" onClick={clearFilters}>{translated.reset}</button>
+                <button className="button button--light form-actions__right" onClick={applyAllFilters}>{translated.filter}</button>
               </div>
-            </>
-          )}
-        </>
-        {/* only show result string if list filtered or searched not just ordered */}
-        {resultString && (
-          <div className="control-bar">
-            {props.numOfResults >= 0 && getResultCountText(props.numOfResults)}
+            </div>
           </div>
-        )}
-        {term && (
-          <ControlBarSearchTerm term={term} onDismiss={() => applySearch('')} />
-        )}
+        </div>
+      </div>
+
+      <div className="form-actions">
+        <div className="form-actions__left">
+          <div>
+            {/* only show result string if list filtered or searched not just ordered */}
+            {appliedFilters.length > 0 && (
+              <div className="control-bar__results">
+                {props.numOfResults >= 0 && getResultCountText(props.numOfResults)}
+              </div>
+            )}
+            <ControlBarFilterPills
+              filters={appliedFilters}
+              onRemove={(type) => handleFilterChange(type, '')}
+            />
+          </div>
+        </div>
+        <div className="form-actions__right">
+          <ControlBarListMapSwitch query={queryParams} />
+        </div>
       </div>
     </nav>
   )

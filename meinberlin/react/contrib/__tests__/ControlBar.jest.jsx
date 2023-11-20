be04bcb07b6a64e449a5d74257bb73ccd663835b
Philipp Veller <mail@vellip.de>
2023-11-20 08:29:37 +0100

    react/controlbar: adapt controlbar to new design and updated functionality
    react/controlbar: PR review fixes
    
    react/controlBarSearch: update to reflect markup of styleguide, add some style fixes

--- meinberlin/react/contrib/__tests__/ControlBar.jest.jsx
+++ meinberlin/react/contrib/__tests__/ControlBar.jest.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import {
+  fireEvent,
+  render,
+  screen
+} from '@testing-library/react'
 import { ControlBar } from '../ControlBar'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -9,6 +13,24 @@ const filters = {
     current: 'all',
     icons: [['1', 'some/url']],
     choices: [['1', 'all'], ['2', 'category1']]
+  },
+  labels: {
+    label: 'labels',
+    current: 'all',
+    icons: [['1', 'some/url']],
+    choices: [['1', 'all'], ['2', 'label1'], ['3', 'label2']]
+  },
+  tasks: {
+    label: 'tasks',
+    current: 'all',
+    icons: [['1', 'some/url']],
+    choices: [['1', 'all'], ['2', 'task1'], ['3', 'task2']]
+  },
+  status: {
+    label: 'status',
+    current: 'all',
+    icons: [['1', 'some/url']],
+    choices: [['1', 'all'], ['2', 'status1'], ['3', 'status2']]
   },
   ordering: {
     label: 'ordering',
@@ -28,13 +50,13 @@ test('ControlBar shown when list is empty', () => {
       />
     </BrowserRouter>
   )
-  const numOfResultsElement = screen.queryByText(/2/)
+  const numOfResultsElement = screen.queryByText(/found/)
   expect(numOfResultsElement).toBeFalsy()
-  const orderingFilterElement = screen.getByText('ordering: Most recent')
+  const orderingFilterElement = screen.getByText('Most recent')
   expect(orderingFilterElement).toBeTruthy()
 })
 
-test('ControlBar collapsed bar', () => {
+test('ControlBar expanded by default', async () => {
   const onChangeFiltersFn = jest.fn()
   render(
     <BrowserRouter>
@@ -45,27 +67,35 @@ test('ControlBar collapsed bar', () => {
       />
     </BrowserRouter>
   )
-  const numOfResultsElement = screen.queryByText(/2/)
-  expect(numOfResultsElement).toBeFalsy()
-  const orderingFilterElement = screen.getByText('ordering: Most recent')
-  expect(orderingFilterElement).toBeTruthy()
-})
-
-test('ControlBar expanded bar', async () => {
-  const onChangeFiltersFn = jest.fn()
-  render(
-    <BrowserRouter>
-      <ControlBar
-        filters={filters}
-        onChangeFilters={() => onChangeFiltersFn()}
-        numOfResults={2}
-      />
-    </BrowserRouter>
-  )
-  const filterButton = screen.getByText(/Show filters/)
-  let expandedFilter = screen.queryByText(/category1/)
-  expect(expandedFilter).toBeFalsy()
+  const categories = screen.getByText(/categories/)
+  const labels = screen.getByText(/labels/)
+  const task = screen.getByText(/tasks/)
+  let status = screen.getByText('status')
+  expect(categories).toBeTruthy()
+  expect(labels).toBeTruthy()
+  expect(task).toBeTruthy()
+  expect(status).toBeTruthy()
+  const filterButton = screen.getByRole('button', { name: /show less/i })
   fireEvent.click(filterButton)
-  expandedFilter = screen.queryByText(/category1/)
-  expect(expandedFilter).toBeTruthy()
+  status = screen.queryByText('status')
+  expect(status).not.toBeTruthy()
+})
+
+test('ControlBar filters create pills', async () => {
+  const onChangeFiltersFn = jest.fn()
+  render(
+    <BrowserRouter>
+      <ControlBar
+        filters={filters}
+        onChangeFilters={() => onChangeFiltersFn()}
+        numOfResults={2}
+      />
+    </BrowserRouter>
+  )
+  const categories = screen.getByLabelText(/categories/)
+  expect(categories).toBeTruthy()
+  fireEvent.change(categories, { target: { value: '2' } })
+  fireEvent.click(screen.getByRole('button', { name: /filter/ }))
+  const category = screen.getAllByText('categories')
+  expect(category).toHaveLength(2)
 })

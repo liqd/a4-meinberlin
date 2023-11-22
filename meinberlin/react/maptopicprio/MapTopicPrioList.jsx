8e2b8276ae19f33d58e773feb3b6a0f970144489
philli-m <5871230+philli-m@users.noreply.github.com>
2023-11-22 14:51:55 +0100

    react/maptopicprio: add entrypoint and tag into template and add component for cardlist and use for maptopicprio + topicprio

--- meinberlin/react/maptopicprio/MapTopicPrioList.jsx
+++ meinberlin/react/maptopicprio/MapTopicPrioList.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import django from 'django'
 import { CardList } from '../contrib/CardList'
 
-export const TopicPrioList = (props) => {
+export const MapTopicPrioList = (props) => {
   const translations = {
     listStr: django.gettext('Map topics list')
   }
 
   return (
     <CardList
-      apiUrl={props.topics_api_url}
+      apiUrl={props.map_topics_api_url}
       listStr={translations.listStr}
     />
   )

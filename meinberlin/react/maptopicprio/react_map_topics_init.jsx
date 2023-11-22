29b94af456f747190d7a1794e4581fc8463ef96a
philli-m <5871230+philli-m@users.noreply.github.com>
2023-11-22 14:51:55 +0100

    react/maptopicprio: add entrypoint and tag into template and add component for cardlist and use for maptopicprio + topicprio

--- meinberlin/react/maptopicprio/react_map_topics_init.jsx
+++ meinberlin/react/maptopicprio/react_map_topics_init.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { widget as ReactWidget } from 'adhocracy4'
-import { TopicPrioList } from './TopicPrioList.jsx'
+import { MapTopicPrioList } from './MapTopicPrioList.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 function init () {
-  ReactWidget.initialise('mb', 'topics',
+  ReactWidget.initialise('mb', 'map-topics',
     function (el) {
       const props = JSON.parse(el.getAttribute('data-attributes'))
       const root = createRoot(el)
       root.render(
         <React.StrictMode>
           <BrowserRouter>
-            <TopicPrioList {...props} />
+            <MapTopicPrioList {...props} />
           </BrowserRouter>
         </React.StrictMode>
       )

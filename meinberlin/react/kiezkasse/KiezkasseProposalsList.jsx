15ae30229ba0de3b0a06d5c6b1b22b4b577f80c2
philli-m <5871230+philli-m@users.noreply.github.com>
2023-11-23 15:35:53 +0100

    react/kiezkasse: add the list component, entrypoint and template tag and test

---
+++ meinberlin/react/kiezkasse/KiezkasseProposalsList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import django from 'django'
+import { CardList } from '../contrib/CardList'
+
+export const KiezkasseProposalsList = (props) => {
+  const translations = {
+    listStr: django.gettext('Proposals list')
+  }
+
+  return (
+    <CardList
+      apiUrl={props.kiezkasse_proposals_api_url}
+      listStr={translations.listStr}
+      cardStatus
+      cardMeta
+    />
+  )
+}

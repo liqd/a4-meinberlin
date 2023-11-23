39c9f76bd9c4eb5e53bf7e61b2913d57de98b35c
philli-m <5871230+philli-m@users.noreply.github.com>
2023-11-23 14:50:03 +0100

    apps/kiezkasse: add api and use idea mixin and filter set

---
+++ meinberlin/apps/kiezkasse/api.py
@@ -0,0 +1,58 @@
+from typing import List
+
+from django.db.models import QuerySet
+from rest_framework.filters import SearchFilter
+
+from adhocracy4.api.mixins import ModuleMixin
+from adhocracy4.modules.models import Module
+from meinberlin.apps.contrib.filters import NoExceptionFilterBackend
+from meinberlin.apps.contrib.filters import OrderingFilterWithDailyRandom
+from meinberlin.apps.ideas.filters import IdeaFilterSet
+
+from ..ideas.api import BaseIdeaViewSet
+from ..ideas.api import IdeaFilterInfoMixin
+from ..ideas.api import PermissionInfoMixin
+from .models import Proposal
+from .serializers import ProposalSerializer
+
+
+class KiezkasseViewSet(
+    ModuleMixin,
+    IdeaFilterInfoMixin,
+    PermissionInfoMixin,
+    BaseIdeaViewSet,
+):
+    model = Proposal
+    serializer_class = ProposalSerializer
+    filter_backends = (
+        NoExceptionFilterBackend,
+        OrderingFilterWithDailyRandom,
+        SearchFilter,
+    )
+    filterset_class = IdeaFilterSet
+
+    ordering_fields = (
+        "created",
+        "comment_count",
+        "positive_rating_count",
+        "dailyrandom",
+    )
+    search_fields = ("name", "ref_number")
+
+    @property
+    def ordering(self) -> List[str]:
+        return ["dailyrandom"]
+
+    def get_permission_object(self) -> Module:
+        return self.module
+
+    def get_queryset(self) -> QuerySet:
+        proposals = (
+            self.model.objects.filter(module=self.module)
+            .annotate_comment_count()
+            .annotate_positive_rating_count()
+            .annotate_negative_rating_count()
+            .annotate_reference_number()
+            .order_by("-created")
+        )
+        return proposals

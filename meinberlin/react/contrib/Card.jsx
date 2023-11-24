76dcb7457613072370b58f88ac46f0c2ed5bb8e7
philli-m <5871230+philli-m@users.noreply.github.com>
2023-11-24 10:24:21 +0100

    react/Card: add BO card hover behavior to react card

--- meinberlin/react/contrib/Card.jsx
+++ meinberlin/react/contrib/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import django from 'django'
 
 import { CardStats } from './CardStats'
@@ -9,12 +9,32 @@ const translated = {
 }
 
 export const Card = (props) => {
-  const { item, permissions, children } = props
+  const { item, idx, permissions, children } = props
+
+  // needed as BO JS library does not work with async libraries
+  const [hover, setHover] = useState()
+
+  const handleClick = () => {
+    document.getElementById('card-link-' + idx).click()
+  }
+
+  const handleMouseEnter = () => {
+    setHover(!hover)
+  }
+
+  const handleMouseLeave = () => {
+    setHover(!hover)
+  }
 
   return (
-    <article className="modul-card" data-add-clickable-area="smart">
+    <article className="modul-card">
       <header className="card__header">
-        <h3 className="title">{item.name}</h3>
+        {/* eslint-disable-next-line */}
+        <h3
+          className="title" onClick={handleClick} onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >{item.name}
+        </h3>
         {children}
       </header>
       <section className="card__footer--light">
@@ -26,7 +46,7 @@ export const Card = (props) => {
           commentCount={item.comment_count}
           voteCount={item.vote_count}
         />
-        <a href={item.url} className="more" data-mainlink="true">{translated.detailsStr}
+        <a href={item.url} id={'card-link-' + idx} className={'more' + (hover ? ' card__link--hover' : '')} data-mainlink="true">{translated.detailsStr}
           <span className="aural">{translated.toStr} {item.name}</span>
         </a>
       </section>

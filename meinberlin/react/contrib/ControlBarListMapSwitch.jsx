5e478e798b2f625559c47be5e49dda89d5dbe133
philli-m <5871230+philli-m@users.noreply.github.com>
2023-11-29 17:12:44 +0100

    react/contrib/ControlBarFilter: add heading to mapswitch as per design and adding choice value instead of filter name

--- meinberlin/react/contrib/ControlBarListMapSwitch.jsx
+++ meinberlin/react/contrib/ControlBarListMapSwitch.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import django from 'django'
 import { useSearchParams } from 'react-router-dom'
 
+const translated = {
+  viewMode: django.gettext('View mode'),
+  viewList: django.gettext('View as list'),
+  list: django.gettext('List'),
+  viewMap: django.gettext('View as map'),
+  map: django.gettext('Map')
+}
+
 export const ControlBarListMapSwitch = () => {
-  // FIXME: to be changed, once Map is in React
+  // FIXME: to be changed, once Map is in React and made more a11y compliant
 
   const [queryParams, setQueryParams] = useSearchParams()
 
@@ -14,22 +22,25 @@ export const ControlBarListMapSwitch = () => {
   }
 
   return (
-    <div className="control-bar__list-map-switch">
-      <div
-        className="button button--light button--fulltone"
-        aria-label={django.gettext('View as list')}
-      >
-        <span className="fa fa-list" aria-hidden="true" />&nbsp;
-        {django.gettext('List')}
+    <div className="control-bar__list-map-switch-container">
+      <span className="text--strong">{translated.viewMode}</span>
+      <div className="control-bar__list-map-switch">
+        <div
+          className="button button--light button--fulltone"
+          aria-label={translated.viewList}
+        >
+          <span className="fa fa-list" aria-hidden="true" />&nbsp;
+          {translated.list}
+        </div>
+        <button
+          className="button button--light"
+          onClick={handleClick}
+          aria-label={translated.viewMap}
+        >
+          <span className="fa fa-map" aria-hidden="true" />&nbsp;
+          {translated.map}
+        </button>
       </div>
-      <button
-        className="button button--light"
-        onClick={handleClick}
-        aria-label={django.gettext('View as map')}
-      >
-        <span className="fa fa-map" aria-hidden="true" />&nbsp;
-        {django.gettext('Map')}
-      </button>
     </div>
   )
 }

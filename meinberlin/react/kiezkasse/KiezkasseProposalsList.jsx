ed22df58541585b0fcc6e7482bcaec8f52b0b44d
philli-m <5871230+philli-m@users.noreply.github.com>
2023-11-29 15:41:07 +0100

    react/contrib/card: move all card components to own folder and update paths

--- meinberlin/react/kiezkasse/KiezkasseProposalsList.jsx
+++ meinberlin/react/kiezkasse/KiezkasseProposalsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import django from 'django'
-import { CardList } from '../contrib/CardList'
+import { CardList } from '../contrib/card/CardList'
 
 export const KiezkasseProposalsList = (props) => {
   const translations = {

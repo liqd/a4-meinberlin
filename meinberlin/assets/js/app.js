fbc5be723a185f8235a07454f37590277acb22f4
philli-m <5871230+philli-m@users.noreply.github.com>
2023-12-13 15:54:40 +0100

    assets/userindicator: update template, add styles and js functionality for new user indicator

--- meinberlin/assets/js/app.js
+++ meinberlin/assets/js/app.js
@@ -4,6 +4,7 @@ import 'select2' // used to select projects in containers
 
 import '../../apps/actions/assets/timestamps.js'
 import '../../apps/newsletters/assets/dynamic_fields.js'
+import '../../apps/users/assets/user_indicator.js'
 
 // map search function
 import 'adhocracy4/adhocracy4/maps/static/a4maps/a4maps_address.js'

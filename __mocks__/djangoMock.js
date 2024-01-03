467e319c84ddb9ea7ef8cf72da7f692f733a3109
philli-m <5871230+philli-m@users.noreply.github.com>
2024-01-03 15:51:12 +0100

    react/CardStats: update stat strings to include plural versions and context for required strings fixes #5370 and fix tests

--- __mocks__/djangoMock.js
+++ __mocks__/djangoMock.js
@@ -2,5 +2,6 @@ module.exports = {
   gettext: (text) => text,
   pgettext: (text) => text,
   ngettext: (text, count) => text + count,
-  interpolate: (text, count) => text + count
+  interpolate: (text, count) => text + count,
+  npgettext: (context, text, count) => text + count
 }

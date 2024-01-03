0f6ebfee1296dd3899efe28d34437b86b73fc2c8
hom3mad3 <8156337+hom3mad3@users.noreply.github.com>
2024-01-03 14:54:54 +0100

    templates/a4modules: update phase-info component according to new design

--- meinberlin/assets/js/swiper_phases.js
+++ meinberlin/assets/js/swiper_phases.js
@@ -1,63 +1,42 @@
-// import { Swiper } from 'swiper'
-/* eslint import/no-unresolved: [2, { ignore: ['swiper/modules'] }] */
-// import { Pagination, A11y } from 'swiper/modules'
-// import django from 'django'
+const createSwiper = ({ rootElement, options }) =>
+// eslint-disable-next-line no-undef
+  new Swiper(rootElement, options)
 
-// function createSwiper (params) {
-//   const { rootElement, options } = params
-//   return new Swiper(rootElement, options)
-// }
+// Initialize Swiper directly with the configuration
+const initSwiper = () => {
+  const config = {
+    rootElement: '.swiper-container',
+    options: {
+      direction: 'horizontal',
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return (
+            '<button class="' + className + '">' + (index + 1) + '</button>'
+          )
+        }
+      },
+      a11y: true
+    }
+  }
 
-// // find index for given identifier in slide
-// // possible identifiers: active, upcoming
-// function getIndexOf (identifier, slides) {
-//   return Array.from(slides).findIndex(slide => {
-//     return slide.querySelector('#' + identifier)
-//   })
-// }
+  createSwiper(config)
+};
 
-// // create specific phase swiper
-// function createPhaseSwiper () {
-//   // config contains container/root element's name
-//   // and all settings, which are needed (see swiper docs)
-//   const config = {
-//     rootElement: '.swiper-phases',
-//     options: {
-//       modules: [A11y, Pagination],
-//       a11y: {
-//         containerMessage:
-//           django.gettext('Slider to go back and forth between phases.'),
-//         paginationBulletMessage:
-//           django.gettext('Go to slide {{index}}')
-//       },
-//       pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//         bulletElement: 'div'
-//       }
-//     }
-//   }
-//   const phaseSwiper = createSwiper(config)
+(function () {
+  // Check if Swiper is already available globally
+  const hasSwiper = typeof Swiper !== 'undefined'
 
-//   // following part is to set initial slide
-//   // FIXME: it is not the right location to invoke the following;
-//   // because double logic (similar as in template), better to set
-//   // flag `initialSlide` in config on creation
-//   const activeSlideIndex = getIndexOf('active-phase', phaseSwiper.slides)
-//   const upcomingSlideIndex = getIndexOf('upcoming-phase', phaseSwiper.slides)
+  // Call swiper initialization if Swiper is available and swiper element found in DOM
+  const hasSwiperContainer = document.querySelector('.swiper-container')
 
-//   if (activeSlideIndex !== -1) {
-//     phaseSwiper.slideTo(activeSlideIndex)
-//   } else if (upcomingSlideIndex !== -1) {
-//     phaseSwiper.slideTo(upcomingSlideIndex)
-//   } else {
-//     const lastFinishedSlideIndex = phaseSwiper.slides.length - 1
-//     phaseSwiper.slideTo(lastFinishedSlideIndex)
-//   }
-// }
-
-// (function () {
-//   // Call swiper initialization if swiper element found in DOM
-//   // const hasPhaseSwiper = document.querySelector('.swiper-phases')
-//   createPhaseSwiper()
-// })()
+  if (hasSwiper && hasSwiperContainer) {
+    initSwiper()
+  }
+})()

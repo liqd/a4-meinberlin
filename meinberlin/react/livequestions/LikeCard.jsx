82027f9001612cad7edab0b0c1991bbc7e0e38dc
Sev Furneaux <sevfurneaux@gmail.com>
2024-12-05 17:03:55 +0000

    react/livequestions: design tweaks

--- meinberlin/react/livequestions/LikeCard.jsx
+++ meinberlin/react/livequestions/LikeCard.jsx
@@ -20,7 +20,7 @@ export default function LikeCard ({
   const likeText = likes.session_like ? likedTag : likesTag
 
   return (
-    <article className="modul-card card">
+    <article className="modul-card card card--like">
       <header className="card__header">
         <h3 className="title title--no-underline">{title}</h3>
         {(category || isOnShortlist) && (
@@ -36,28 +36,30 @@ export default function LikeCard ({
           </div>
         )}
       </header>
-      {onLikeClick
-        ? (
-          <button
-            type="button"
-            className={classNames(
-              'rating-button',
-              likes.session_like && 'rating-button--active'
+      <div className="card__like">
+        {onLikeClick
+          ? (
+            <button
+              type="button"
+              className={classNames(
+                'rating-button',
+                likes.session_like && 'rating-button--active'
+              )}
+              onClick={onLikeClick}
+              aria-label={likes.session_like ? addLikeTag : undoLikeTag}
+            >
+              <i className="far fa-thumbs-up mr-1" aria-hidden="true" />
+              {likeText} ({likes.count})
+            </button>
+            )
+          : (
+            <span className="rating-button">
+              <i className="far fa-thumbs-up" aria-hidden="true" />
+              {likeText} ({likes.count})
+            </span>
             )}
-            onClick={onLikeClick}
-            aria-label={likes.session_like ? addLikeTag : undoLikeTag}
-          >
-            <i className="far fa-thumbs-up" aria-hidden="true" />
-            {likeText} ({likes.count})
-          </button>
-          )
-        : (
-          <span className="rating-button">
-            <i className="far fa-thumbs-up" aria-hidden="true" />
-            {likeText} ({likes.count})
-          </span>
-          )}
-      <div className="card__header">
+      </div>
+      <div className="card__tools">
         {children}
       </div>
     </article>

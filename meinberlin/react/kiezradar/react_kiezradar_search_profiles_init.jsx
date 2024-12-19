de6a04c45359b03d79073b62724041785c133d60
Sev Furneaux <sevfurneaux@gmail.com>
2024-12-19 13:10:16 +0000

    react/kiez-search-profile: add initial dashboard

--- meinberlin/react/kiezradar/react_kiezradar_search_profiles_init.jsx
+++ meinberlin/react/kiezradar/react_kiezradar_search_profiles_init.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { widget as ReactWidget } from 'adhocracy4'
-import Dummy from './dummy'
+import SearchProfiles from './SearchProfiles'
 
 function init () {
-  ReactWidget.initialise('mb', 'kiezradar-search-profiles',
-    function (el) {
-      // const props = JSON.parse(el.getAttribute('data-attributes'))
-      const root = createRoot(el)
-      root.render(
-        <React.StrictMode>
-          <Dummy />
-        </React.StrictMode>
-      )
-    }
-  )
+  ReactWidget.initialise('mb', 'kiezradar-search-profiles', function (el) {
+    const props = JSON.parse(el.getAttribute('data-attributes'))
+    const root = createRoot(el)
+    root.render(
+      <React.StrictMode>
+        <SearchProfiles {...props} />
+      </React.StrictMode>
+    )
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init, false)

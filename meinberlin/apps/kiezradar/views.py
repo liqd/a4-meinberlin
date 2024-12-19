278dcee67401c5013f9c8ef04fe6aa9901ffee35
Sev Furneaux <sevfurneaux@gmail.com>
2024-12-19 13:10:16 +0000

    react/kiez-search-profile: add initial dashboard

--- meinberlin/apps/kiezradar/views.py
+++ meinberlin/apps/kiezradar/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
+from rules.contrib.views import LoginRequiredMixin
 
 
-class SearchProfileListView(generic.TemplateView):
+class SearchProfileListView(LoginRequiredMixin, generic.TemplateView):
     template_name = "meinberlin_kiezradar/search_profile_list.html"

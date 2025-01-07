4b98e96e6502f44a19d58a494c3c6cc302c2552f
Philipp Veller <mail@vellip.de>
2025-01-07 12:31:44 +0100

    project-tiles: adapt ui states for ended and upcoming projects as well as plans

--- meinberlin/react/projects/ProjectTopics.jsx
+++ meinberlin/react/projects/ProjectTopics.jsx
@@ -1,13 +1,22 @@
+import django from 'django'
 import React from 'react'
 
-const ProjectTopics = ({ project, topicChoices }) => {
+const planStr = django.gettext('Plan')
+
+const ProjectTilePills = ({ project, topicChoices }) => {
   const topicList = project.topics.map((val) => {
     return topicChoices[val]
   })
 
+  if (project.type === 'plan' && topicList.length > 1) {
+    const additionalPills = topicList.length - 1
+    topicList.splice(1, additionalPills, '+1')
+  }
+
   if (project.topics && project.topics.length > 0) {
     return (
       <ul className="pill__list pill__list--inline">
+        {project.type === 'plan' && <li className="pill pill--label">{planStr}</li>}
         {topicList.map(topic =>
           <li key={topic} className="pill pill--topic">{topic}</li>
         )}
@@ -16,4 +25,4 @@ const ProjectTopics = ({ project, topicChoices }) => {
   }
 }
 
-export default ProjectTopics
+export default ProjectTilePills

b3826a488812af9e3cda95e75c8840614c4b72cf
Sev Furneaux <sevfurneaux@gmail.com>
2025-01-15 18:54:31 +0000

    search-profile: add search profile button

---
+++ meinberlin/react/kiezradar/use-create-search-profile.jest.js
@@ -0,0 +1,141 @@
+import { renderHook, act } from '@testing-library/react'
+import { updateItem } from '../contrib/helpers'
+import { useCreateSearchProfile } from './use-create-search-profile'
+
+jest.mock('../contrib/helpers', () => ({
+  updateItem: jest.fn()
+}))
+
+describe('useCreateSearchProfile', () => {
+  const searchProfilesApiUrl = '/api/search-profiles'
+  const districts = [
+    { id: 1, name: 'Charlottenburg-Wilmersdorf' },
+    { id: 2, name: 'Friedrichshain-Kreuzberg' }
+  ]
+  const organisations = [{ id: 1, name: 'liqd' }]
+  const topicChoices = [
+    { id: 1, name: 'Anti-discrimination, Work & economy', code: 'ANT' }
+  ]
+  const participationChoices = [
+    { id: 1, name: 'information (no participation)' },
+    { id: 2, name: 'consultation' },
+    { id: 3, name: 'cooperation' },
+    { id: 4, name: 'decision-making' }
+  ]
+  const projectStatus = [
+    {
+      id: 1,
+      status: 0,
+      name: 'running'
+    },
+    {
+      id: 2,
+      status: 1,
+      name: 'done'
+    },
+    {
+      id: 3,
+      status: 2,
+      name: 'future'
+    }
+  ]
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('handles submission of a search profile', async () => {
+    const appliedFilters = {
+      districts: ['Charlottenburg-Wilmersdorf', 'Friedrichshain-Kreuzberg'],
+      organisation: ['liqd'],
+      topics: ['ANT'],
+      participations: [0, 1, 2],
+      projectState: ['active', 'past', 'future'],
+      search: ''
+    }
+
+    const mockedData = {
+      districts: [1, 2],
+      organisations: [1],
+      topics: [1],
+      project_types: [1, 2, 3],
+      status: [1, 2, 3]
+    }
+
+    const { result } = renderHook(() =>
+      useCreateSearchProfile({
+        searchProfilesApiUrl,
+        appliedFilters,
+        districts,
+        organisations,
+        topicChoices,
+        participationChoices,
+        projectStatus,
+        onSearchProfileCreate: () => {}
+      })
+    )
+
+    await act(async () => {
+      await result.current.createSearchProfile()
+    })
+
+    expect(updateItem).toHaveBeenCalledWith(
+      expect.objectContaining(mockedData),
+      searchProfilesApiUrl,
+      'POST'
+    )
+  })
+
+  it('calls onSearchProfileCreate with searchProfile from updateItem', async () => {
+    const appliedFilters = {
+      districts: [],
+      organisation: [],
+      topics: [],
+      participations: [],
+      projectState: [],
+      search: ''
+    }
+
+    const mockedSearchProfile = {
+      id: 1,
+      user: 1,
+      name: 'Searchprofile 1',
+      description: null,
+      disabled: false,
+      notification: false,
+      status: [],
+      query: 15,
+      organisations: [],
+      districts: [],
+      project_types: [],
+      topics: [],
+      query_text: ''
+    }
+
+    jest.mocked(updateItem).mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(mockedSearchProfile)
+    })
+
+    const mockOnSearchProfileCreate = jest.fn()
+
+    const { result } = renderHook(() =>
+      useCreateSearchProfile({
+        searchProfilesApiUrl,
+        appliedFilters,
+        districts,
+        organisations,
+        topicChoices,
+        participationChoices,
+        projectStatus,
+        onSearchProfileCreate: mockOnSearchProfileCreate
+      })
+    )
+
+    await act(async () => {
+      await result.current.createSearchProfile()
+    })
+
+    expect(mockOnSearchProfileCreate).toHaveBeenCalledWith(mockedSearchProfile)
+  })
+})
---
+++ meinberlin/react/kiezradar/use-create-search-profile.js
@@ -0,0 +1,127 @@
+import { useState } from 'react'
+import django from 'django'
+import { updateItem } from '../contrib/helpers'
+
+const errorCreatingSearchProfile = django.gettext(
+  'Error creating search profile'
+)
+
+const STATUS_MAPPING = {
+  running: 'active',
+  done: 'past',
+  future: 'future'
+}
+
+export function useCreateSearchProfile ({
+  searchProfilesApiUrl,
+  appliedFilters,
+  districts,
+  organisations,
+  topicChoices,
+  participationChoices,
+  projectStatus,
+  onSearchProfileCreate
+}) {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const createSearchProfile = async () => {
+    setLoading(true)
+    setError(null)
+
+    const results = getFilteredResults({
+      appliedFilters,
+      districts,
+      organisations,
+      topicChoices,
+      participationChoices,
+      projectStatus
+    })
+
+    const {
+      districtIds,
+      organisationIds,
+      topicIds,
+      participationIds,
+      projectStatusIds
+    } = getFilteredIds(results)
+
+    const params = {
+      districts: districtIds,
+      organisations: organisationIds,
+      topics: topicIds,
+      project_types: participationIds,
+      status: projectStatusIds
+    }
+
+    if (appliedFilters.search.length > 0) {
+      params.query_text = appliedFilters.search
+    }
+
+    try {
+      const response = await updateItem(params, searchProfilesApiUrl, 'POST')
+      const searchProfile = await response.json()
+
+      onSearchProfileCreate(searchProfile)
+    } catch (err) {
+      setError(errorCreatingSearchProfile)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { loading, error, createSearchProfile }
+}
+
+function getFilteredResults ({
+  appliedFilters,
+  districts,
+  organisations,
+  topicChoices,
+  participationChoices,
+  projectStatus
+}) {
+  return {
+    filteredDistricts: districts.filter(district =>
+      appliedFilters.districts.includes(district.name)
+    ),
+    filteredOrganisations: organisations.filter(organisation =>
+      appliedFilters.organisation.includes(organisation.name)
+    ),
+    filteredTopics: topicChoices.filter(topic =>
+      appliedFilters.topics.includes(topic.code)
+    ),
+    filteredParticipationChoices: appliedFilters.participations.map(index =>
+      participationChoices[index]
+    ),
+    filteredProjectStatus: projectStatus.filter(status =>
+      appliedFilters.projectState.includes(STATUS_MAPPING[status.name])
+    )
+  }
+}
+
+function getFilteredIds (results) {
+  const filters = [
+    results.filteredDistricts,
+    results.filteredOrganisations,
+    results.filteredTopics,
+    results.filteredParticipationChoices,
+    results.filteredProjectStatus
+  ]
+
+  const [
+    districtIds,
+    organisationIds,
+    topicIds,
+    participationIds,
+    projectStatusIds
+  ] = filters.map(items => items.map(item => item.id))
+
+  return {
+    districtIds,
+    organisationIds,
+    topicIds,
+    participationIds,
+    projectStatusIds
+  }
+}

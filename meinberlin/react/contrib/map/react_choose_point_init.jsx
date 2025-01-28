d99c9f9de162807a17c6cc2cd84971464b071bf5
Philipp Veller <mail@vellip.de>
2025-01-28 09:47:19 +0100

    maps/react/choose-point: Add forwardRef to mb base map; use a4s new SearchAndShowAddress & ChoosePointMap

--- meinberlin/react/contrib/map/react_choose_point_init.jsx
+++ meinberlin/react/contrib/map/react_choose_point_init.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { widget as ReactWidget } from 'adhocracy4'
 import { Map } from './Map'
-import AddMarkerControl
-  from 'adhocracy4/adhocracy4/maps_react/static/a4maps_react/AddMarkerControl'
+import ChoosePointMap
+  from 'adhocracy4/adhocracy4/maps_react/static/a4maps_react/ChoosePointMap'
 
 function init () {
   ReactWidget.initialise('a4', 'react-choose-point', function (el) {
@@ -11,9 +11,12 @@ function init () {
     const root = createRoot(el)
     root.render(
       <React.StrictMode>
-        <Map {...props.map} id="choose-point">
-          <AddMarkerControl input={el.nextElementSibling} point={props.map.point} />
-        </Map>
+        <ChoosePointMap
+          BaseMap={Map}
+          input={el.nextElementSibling}
+          apiUrl="https://bplan-prod.liqd.net/api/addresses/"
+          {...props.map}
+        />
       </React.StrictMode>
     )
   })

40d5f128955693d2c86badf6eecf05b17b30cad3
Sev Furneaux <sevfurneaux@gmail.com>
2025-02-20 23:59:19 +0000

    Rename notifications-react

--- meinberlin/react/account/react_notification_settings.jsx
+++ meinberlin/react/account/react_notification_settings.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import NotificationsList from './NotificationsList'
 
 function init () {
-  const el = document.getElementById('notifications-react')
+  const el = document.getElementById('notification-settings-react')
   const root = createRoot(el)
   const notifications = JSON.parse(el.getAttribute('data-initial-notifications'))
   const showRestricted = JSON.parse(el.getAttribute('data-show-restricted'))

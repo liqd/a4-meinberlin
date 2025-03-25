6fc356183ed42548cdbda2d14c130fc995fc4709
Philipp Veller <mail@vellip.de>
2025-03-25 10:40:18 +0100

    tiles/hero: add placeholder images

--- meinberlin/react/contrib/ImageWithPlaceholder.jsx
+++ meinberlin/react/contrib/ImageWithPlaceholder.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 
 const ImageWithPlaceholder = ({ src, alt, lazy = true, ...rest }) => {
   const hasImage = src && src.length
-  return (
-    <img
-      src={hasImage ? src : '/static/images/placeholder.png'}
-      alt={alt}
-      loading={lazy ? 'lazy' : 'eager'}
-      {...rest}
-    />
-  )
+  return hasImage
+    ? (
+      <img
+        src={src}
+        alt={alt}
+        loading={lazy ? 'lazy' : 'eager'}
+        {...rest}
+      />
+      )
+    : (
+      <picture>
+        <source type="image/webp" srcSet="/static/images/placeholder_tile.webp" />
+        <source type="image/avif" srcSet="/static/images/placeholder_tile.avif" />
+        <img
+          src="/static/images/placeholder_tile.svg"
+          alt={alt}
+          loading={lazy ? 'lazy' : 'eager'}
+          {...rest}
+        />
+      </picture>
+      )
 }
 
 export default ImageWithPlaceholder

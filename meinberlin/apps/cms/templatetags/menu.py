48e29880681e97bdcca7e64bf6c6475bfbd5ec1e
Sev Furneaux <sevfurneaux@gmail.com>
2025-03-28 12:59:22 +0000

    add menu notification counter

--- meinberlin/apps/cms/templatetags/menu.py
+++ meinberlin/apps/cms/templatetags/menu.py
@@ -1,6 +1,7 @@
 from django import template
 
 from meinberlin.apps.cms import models as cms_models
+from meinberlin.apps.notifications.models import Notification
 
 register = template.Library()
 
@@ -11,6 +12,21 @@ def get_header_menu(title):
 
     if menu is not None:
         return menu.items.all()
+
+
+@register.simple_tag(takes_context=True)
+def get_notifications_count(context):
+    request = context.get("request")
+    if not request or not request.user.is_authenticated:
+        return 0
+
+    unread_count = (
+        Notification.objects.filter(recipient=request.user, read=False)
+        .exclude(action__actor=request.user)
+        .count()
+    )
+
+    return unread_count
 
 
 @register.simple_tag

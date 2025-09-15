31905c614f934aff776d5f737c2c0a21fd046d7b
Josh <195121232+partizipation@users.noreply.github.com>
2025-09-15 17:51:34 +0200

    add squashed changes

--- meinberlin/react/projects/ProjectsList.jsx
+++ meinberlin/react/projects/ProjectsList.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
+import django from 'django'
 import ProjectTile from './ProjectTile'
 import { classNames } from 'adhocracy4'
 
-const ProjectsList = ({ projects, topicChoices, isHorizontal }) => {
-  if (projects.length > 0) {
-    return (
-      <ul className={classNames('projects-list', isHorizontal ? 'projects-list--horizontal' : 'projects-list--vertical')}>
-        {projects.map((project) => (
-          <li key={project.id}>
+const translated = {
+  showCompletedProjectsHeading: django.gettext('Show completed projects too?'),
+  showCompletedProjectsBody: django.gettext('Expand your search to view completed projects and discover more possibilities.'),
+  showCompletedProjectsButton: django.gettext('Show completed')
+}
+
+const ProjectsList = ({
+  projects,
+  topicChoices,
+  isHorizontal,
+  searchCompletedProjects,
+  showSearchCompletedProjectsButton
+}) => {
+  return (
+    <ul className={classNames('projects-list', isHorizontal ? 'projects-list--horizontal' : 'projects-list--vertical')}>
+      {
+        projects.length > 0 &&
+        projects.map((project, index) => (
+          <li key={'project-' + project.id + '-' + index}>
             <ProjectTile
               project={project}
               isHorizontal={isHorizontal}
               topicChoices={topicChoices}
             />
           </li>
-        ))}
-      </ul>
-    )
-  } else {
-    return null
-  }
+        ))
+      }
+      {
+        showSearchCompletedProjectsButton && (
+          <li key="show-completed-projects">
+            <h3 className="projects-list__showCompletedHeading">{translated.showCompletedProjectsHeading}</h3>
+            <p>
+              {translated.showCompletedProjectsBody}
+            </p>
+            <button
+              onClick={searchCompletedProjects}
+              className="button button--light"
+              type="button"
+            >
+              {translated.showCompletedProjectsButton}
+            </button>
+          </li>
+        )
+      }
+    </ul>
+  )
 }
+
 export default ProjectsList

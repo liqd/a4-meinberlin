5c4533eeeef65cad05ed6d72a1757395b19a3b65
glanzel <glanzel@gmx.de>
2025-12-08 17:45:21 +0100

    [ST-687] apps/offlineevent: Delete deprecated Offline Events (#6579)
    + ui improvements

--- meinberlin/apps/offlineevents/forms.py
+++ meinberlin/apps/offlineevents/forms.py
@@ -6,37 +6,6 @@ from adhocracy4.forms.fields import DateTimeField
 
 from . import models
 
-
-class OfflineEventForm(forms.ModelForm):
-    date = DateTimeField(
-        time_format="%H:%M",
-        required=True,
-        require_all_fields=False,
-        label=(_("Date"), _("Time")),
-    )
-
-    class Meta:
-        model = models.OfflineEvent
-        fields = ["name", "event_type", "date", "description"]
-        help_texts = {
-            "description": _(
-                "If you add an image, please provide an alternate text. "
-                "It serves as a textual description of the image content "
-                "and is read out by screen readers. Describe the image "
-                "in approx. 80 characters. Example: A busy square with "
-                "people in summer."
-            ),
-            "event_type": _(
-                "Please describe in no more than 30 characters the event "
-                "type, e.g. Information event or 3rd public workshop."
-            ),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["date"].label = _("Date and time")
-
-
 # Module Area
 
 
@@ -61,21 +30,9 @@ class OfflineEventBasicForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["name"].required = True
+        self.fields["name"].label = _("Name of event")
         self.fields["event_type"].required = True
-
-    help_texts = {
-        "description": _(
-            "If you add an image, please provide an alternate text. "
-            "It serves as a textual description of the image content "
-            "and is read out by screen readers. Describe the image "
-            "in approx. 80 characters. Example: A busy square with "
-            "people in summer."
-        ),
-        "event_type": _(
-            "Please describe in no more than 30 characters the event "
-            "type, e.g. Information event or 3rd public workshop."
-        ),
-    }
+        self.fields["description"].required = True
 
     class Meta:
         from adhocracy4.modules import models as module_models
@@ -86,4 +43,17 @@ class OfflineEventBasicForm(forms.ModelForm):
             "name",
             "description",
         ]
+        help_texts = {
+            "description": _(
+                "If you add an image, please provide an alternate text. "
+                "It serves as a textual description of the image content "
+                "and is read out by screen readers. Describe the image "
+                "in approx. 80 characters. Example: A busy square with "
+                "people in summer."
+            ),
+            "event_type": _(
+                "Please describe in no more than 30 characters the event "
+                "type, e.g. Information event or 3rd public workshop."
+            ),
+        }
         required_for_project_publish = "__all__"

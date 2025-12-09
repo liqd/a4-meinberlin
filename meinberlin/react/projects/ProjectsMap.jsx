cc6502fcf641005ed42125b302b454ab22db200a
Shane <s.obrien@liqd.net>
2025-12-09 15:35:55 +0100

    a11y fix: Fix popup aria info for kiezradar map: p125 u4s2

--- meinberlin/react/projects/ProjectsMap.jsx
+++ meinberlin/react/projects/ProjectsMap.jsx
@@ -24,9 +24,9 @@ const Markers = ({ items, topicChoices }) => {
     items
       .filter(item => !!item.point)
       .map(item => ({ ...item.point, properties: item }))
-      .map((project, index) => (
+      .map((project) => (
         <ProjectMarker
-          key={project.properties.title + index}
+          key={project.properties.title}
           topicChoices={topicChoices}
           project={project}
           onOpen={() => setActiveProject(project)}
@@ -59,6 +59,14 @@ const ProjectsMap = ({ items, topicChoices, ...props }) => {
   return (
     <div className="projects-map">
       <ProjectsMapInfo className="projects-map-info--mobile" />
+      <div
+        id="map-live-region"
+        aria-live="polite"
+        aria-atomic="true"
+        className="visually-hidden"
+      >
+        {/* Screen reader announcements will go here */}
+      </div>
       <Map
         zoomControl={false}
         maxZoom={18}
